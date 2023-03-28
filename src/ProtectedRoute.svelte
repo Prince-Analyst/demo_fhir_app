24cbb3890b0326b3569c30095f3e3b7c338a4289
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-03-28 00:11:49 -0700

    updated package list

---
+++ src/ProtectedRoute.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Route } from "svelte-routing";
+    import accessDenied from "./components/accessDenied.svelte";
+    import { token } from "./stores";
+    export let path;
+    export let component;
+    import { onMount } from "svelte";
+    import { registerAxios } from "./auth";
+    onMount(() => {
+      if ($token) {
+        registerAxios($token);
+      }
+    });
+  </script>
+  
+  {#if $token}
+    <Route {path} {component} />
+  {:else}
+    <Route {path} component={accessDenied} />
+  {/if}
+  
