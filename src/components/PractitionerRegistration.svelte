3abc0995cdb5f427d449e4fd7ad4b2d3bf66a99a
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-04-16 23:33:46 +0100

    updated patient registration page

--- src/components/PractitionerRegistration.svelte
+++ src/components/PractitionerRegistration.svelte
@@ -1,190 +1,222 @@
-<script lang="ts" src="https://unpkg.com/medblocks-ui@0.0.82/dist/bundle.js">
-  import 'medblocks-ui';
-  import "medblocks-ui/dist/shoelace";
-  import '@shoelace-style/shoelace/dist/themes/light.css';
-  import './tailwind.css'
-  import { FHIR, openehr } from '../fhir';
-  import { onMount } from 'svelte';
-  //import { expect, oneEvent } from '@open-wc copy/testing';
-  //import { fixture } from '@open-wc copy/testing-helpers';
-  //import { html } from 'lit-html';
-  //import MbForm from '../fhirForm';
+<script lang="ts">
+  import "medblocks-ui/dist/medblocks";
+  import { onMount } from "svelte";
+  import { fhir } from "../fhir";
+  import { Link, navigate } from "svelte-routing";
 
-  let loading: boolean = false;
-  export let id = undefined;
-  //export let ehrId = undefined;
+  let data = {};
   let form;
-  console.log(id);
-  async function handleSubmit(e: any){
-    loading = true;
-
-  document.addEventListener('DOMContentLoaded', () => {
-    var form = document.getElementById('fhir-form');
-    
-    form.addEventListener('mb-submit', event => {console.log(e.detail)})
-    
-    });
-
-    //const resp = await FHIR.post("/Practitioner", e.detail);
-    //if (resp.status == 201) {
-    //    ehrId = resp.data.id;
-    //}
-    //const respEHR = await openehr.put(`/ehr/${ehrId}`);
-
-    if (id) {
-        const resp = await FHIR.put(`/Practitioner/${id}`, { ...e.detail, id })
-        //if (resp.status === 200) {
-        //  const ehrId = resp.data.id;
-        //  const respEHR = await openehr.put(`/ehr/${ehrId}/composition`)
-        //}
-        console.log(resp.data);
-    } else {
-        const resp = await FHIR.post("/Practitioner", e.detail);
-        //if (resp.status === 201) {
-        //  var ehrId = resp.data.id;
-        //  const respEHR = await openehr.post(`/ehr/${ehrId}/composition`);
-        //}
-        console.log(resp.data);
-    }
-    loading = false;
-  }
+  let loading = false;
+  export let id;
 
   onMount(async () => {
-    if (id){
-        const resp = await FHIR.get(`/Practitioner/${id}`)
-        //if (resp.status === 200) {
-        //  const ehrId = resp.data.id;
-        //  const respEHR = await openehr.get(`/ehr/${ehrId}`);
-        //}
-        const resource = resp.data
-        form.import(resource)
+    if (id) {
+      const r = await fhir.get(`/Patient/${id}`);
+      console.log(r.data);
+      console.log(form.parse(r.data));
+      data = form.parse(r.data);
     }
-
-  })
-  //};
-
+  });
+  const handleSubmit = async (e: any) => {
+    const data = e.detail;
+    console.log(data);
+    if (id) {
+      loading = true;
+      await fhir.put(`/Patient/${id}`, { ...data, id });
+      loading = false;
+    } else {
+      loading = true;
+      await fhir.post(`/Patient`, data);
+      loading = false;
+    }
+    navigate("/", { replace: true });
+  };
 </script>
-  
-  
-  
-  <h1 class="text-2x font-semibold font-sans">Practitioner Registration</h1>
-  <mb-fhir-form 
-      id="form"
-      bind:this={form} 
-      class="flex flex-col gap-3" 
-      on:mb-submit={handleSubmit}
-  >
-    <div class="field">
-      <mb-context  path="resourceType" bind="Practitioner" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-checkbox type="boolean" path="active" label="Active" />
-    </div>
-    <div class="field">
-      <mb-context type="HumanName" path="name[0].use[0]" bind="official" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="HumanName" path="name[0].prefix" placeholder="Prefix" />
-    </div>
-    <br>
-    <div class="filed">
-      <mb-input type="HumanName" path="name[0].given" placeholder="First Name" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="HumanName" path="name[0].family" placeholder="Last Name" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="HumanName" path="name[0].suffix" placeholder="Suffix" />
-    </div>
-    <div class="field mb-5">
-      <mb-date type="date" label="Date of Birth" placeholder="Date of Birth" path="birthDate" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-buttons type="code" path="gender" label="Gender" >
-        <mb-option value="male" label="Male" />
-        <mb-option value="female" label="Female" />
-        <mb-option value="other" label="Other" />
-      </mb-buttons>
-    </div>
-    <div class="field">
-      <mb-context type="ContactPoint" path="telecom[0].system[0]" bind="phone">
-    </div>
-    <div class="field">
-      <mb-context type="ContactPoint" path="telecom[0].use[0]" bind="mobile" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="ContactPoint" path="telecom[0].value[0]" placeholder="Phone Number" />
-    </div>
-    <div class="field">
-      <mb-context type="ContactPoint" path="telecom[1].system[0]" bind="email">
-    </div>
-    <div class="field">
-      <mb-context type="ContactPoint" path="telecom[1].use[0]" bind="mobile" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="ContactPoint" path="telecom[1].value[0]" placeholder="Email Address" />
-    </div>
-    <div class="field"> 
-      <mb-context type="	Address" path="address[0].use" bind="home" />
-    </div>
-    <div class="field">
-      <mb-context type="	Address" path="address[0].type" bind="both" />
-    </div>
-    <br>
-    <div class="field" >
-      <mb-input type="	Address" path="address[0].line" placeholder="Line 1" />
-    </div>
-    <br>
-    <div class="field" >
-      <mb-input type="Address" path="address[0].line" placeholder="Line 2" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="	Address" path="address[0].city" placeholder="City"/>
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="	Address" path="address[0].state" placeholder="State" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="	Address" path="address[0].postalCode" placeholder="Postal Code" />
-    </div>
-    <br>
-    <div class="field">
-      <mb-input type="	Address" path="address[0].country" placeholder="Country" />
-    </div>
-    <br>
-    <div class="field mb-5">
-      <mb-select type="code" path="communication.coding[0]" placeholder="Communication Language">
-        <mb-option value="ar" label="Arabic" />
-        <mb-option value="en" label="English" />
-        <mb-option value="fr" label="French" />
-        <mb-option value="hi" label="Hindi" />
-        <mb-option value="it" label="Italian" />
-        <mb-option value="pt" label="Portuguese" />
-        <mb-option value="sv" label="Swedish" />
-        <mb-option value="zh" label="Chinese" />
-        <mb-option value="ja" label="Japanese" />
-        <mb-option value="de" label="German" />
-        <mb-option value="es" label="Spanish" />
-      </mb-select>
-    </div>
-    <!-- <br> -->
-    <!-- <div> -->
-    <!--   <mb-multimedia type="upload" label="Photo" path="photo" /> -->
-    <!-- </div> -->
-    <br>
-    <div>
-      <mb-submit>
-        <sl-button {loading} class="text-blue-600 text-4xl font-bold" type="info">Submit</sl-button>
-      </mb-submit>
-    </div>
-  </mb-fhir-form>
+
+<h1 class="text-2xl mb-5 font-bold text-gray-700">Patient registration</h1>
+
+<mb-fhir-form
+  class="flex flex-col gap-3"
+  {data}
+  bind:this={form}
+  on:mb-input={(e) => {
+    data = e.target.data;
+  }}
+  on:mb-submit={handleSubmit}
+>
+  <div class="field">
+    <mb-input class="hidden" path="resourceType" data="Patient" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-checkbox type="boolean" path="active" label="Active" />
+  </div>
+  <div class="field">
+    <mb-context type="HumanName" path="name[0].use[0]" bind="official" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="HumanName" path="name[0].prefix" placeholder="Prefix" />
+  </div>
+  <br>
+  <div class="filed">
+    <mb-input type="HumanName" path="name[0].given" placeholder="First Name" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="HumanName" path="name[0].family" placeholder="Last Name" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="HumanName" path="name[0].suffix" placeholder="Suffix" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-date datatype="date" label="Date of birth" path="birthDate" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-buttons datatype="code" label="Gender" path="gender">
+      <mb-option value="male" label="Male" />
+      <mb-option value="female" label="Female" />
+      <mb-option value="other" label="Other" />
+    </mb-buttons>
+  </div>
+  <div class="field">
+    <mb-checkbox type="boolean" path="deceasedBoolean" label="Deceased" />
+  </div>
+  <br>
+  <div>
+    <mb-date time type="dateTime" path="deceasedDateTime" placeholder="Date of Death" label="Date and Time of Death" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-select type="code" path="maritalStatus.coding[0]" placeholder="Marital Status" >
+      <mb-option value="A" label="Annulled" display="Annulled" />
+      <mb-option value="D" label="Divorced" display="Divorced" />
+      <mb-option value="I" label="Interlocutory" display="Interlocutory" />
+      <mb-option value="L" label="Legally Separated" display="Legally Separated" />
+      <mb-option value="M" label="Married" display="Married" />
+      <mb-option value="C" label="Common Law" display="Common Law" />
+      <mb-option value="P" label="Polygamous" display="Polygamous" />
+      <mb-option value="T" label="Domestic Partner" display="Domestic Partner" />
+      <mb-option value="U" label="Unmarried" display="Unmarried" />
+      <mb-option value="S" label="Never Married" display="Never Married" />
+      <mb-option value="W" label="Widowed" display="Widowed" />
+    </mb-select>
+  </div>
+  <div class="field">
+    <mb-context type="CodeableConcept" path="maritalStatus.coding[0].system[0]" bind="http://terminology.hl7.org/CodeSystem/v3-MaritalStatus" />
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="telecom[0].system[0]" bind="phone">
+  </div>
+
+  <div class="field">
+    <mb-context type="ContactPoint" path="telecom[0].use[0]" bind="mobile" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="ContactPoint" path="telecom[0].value[0]" placeholder="Phone Number" />
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="telecom[1].system[0]" bind="email">
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="telecom[1].use[0]" bind="mobile" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="ContactPoint" path="telecom[1].value[0]" placeholder="Email Address" />
+  </div>
+  <br>
+  <mb-select datatype="code" label="Identifier type" path="identifier[0].system">
+    <mb-option value="passport" label="Passport Number" />
+    <mb-option value="driving" label="Driving License" />
+    <mb-option value="nin" label="NIN" />
+    <mb-option value="voter" label="Voter ID" />
+  </mb-select>
+  <mb-input path="identifier[0].value" label="Identifier number" />
+  <div class="field">
+    <mb-context type="Address" path="address[0].use" bind="home" />
+  </div>
+  <div class="field">
+    <mb-context type="Address" path="address[0].type" bind="physical" />
+  </div>
+  <div class="field" >
+    <mb-input type="Address" path="address[0].line" placeholder="Line 1" />
+  </div>
+  <br>
+  <div class="field" >
+    <mb-input type="Address" path="address[0].line" placeholder="Line 2" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="Address" path="address[0].city" placeholder="City"/>
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="Address" path="address[0].state" placeholder="State" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="Address" path="address[0].postalCode" placeholder="Postal Code" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="Address" path="address[0].country" placeholder="Country" />
+  </div>
+  <div class="field">
+    <mb-input label="Address" textarea path="address[0].text" />
+  </div>
+  <div class="field">
+    <label for="" class="font-bold">Attendant information</label>
+  </div>
+  <div class="flex flex-col gap-4">
+    <mb-input label="Attendant name" path="contact[0].name.given[0]" />
+  </div>
+  <div class="field">
+    <mb-select
+    label="Attendant relationship"
+    path="contact[0].relationship[0]"
+    datatype="CodableConcept"
+    >
+    <mb-option value="mother" label="Mother" />
+    <mb-option value="father" label="Father" />
+    <mb-option value="daughter" label="Daughter" />
+    <mb-option value="son" label="Son" />
+    <mb-option value="brother" label="Brother" />
+    <mb-option value="sister" label="Sister" />
+    <mb-option value="husband" label="Husband" />
+    <mb-option value="wife" label="Wife" />
+    <mb-option value="neighbor" label="Neighbor" />
+    <mb-option value="other" label="Other" />
+  </mb-select>
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="contact[0].telecom[0].system[0]" bind="phone">
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="contact[0].telecom[0].use[0]" bind="mobile" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="ContactPoint" path="contact[0].telecom[0].value[0]" placeholder="Attendant Phone Number" />
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="contact[0].telecom[1].system[0]" bind="email">
+  </div>
+  <div class="field">
+    <mb-context type="ContactPoint" path="contact[0].telecom[1].use[0]" bind="mobile" />
+  </div>
+  <br>
+  <div class="field">
+    <mb-input type="ContactPoint" path="contact[0].telecom[1].value[0]" placeholder="Attendant Email Address" />
+  </div>
+  <br>
+  <mb-submit>
+    <sl-button size="large" class="mt-4 w-full" {loading} type="neutral">
+      Save
+    </sl-button>
+  </mb-submit>
+</mb-fhir-form>
